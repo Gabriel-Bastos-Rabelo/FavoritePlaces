a890dc6468930fb5de8ace614111f47fe609906f
Gabriel Bastos <gbastosrabelo@gmail.com>
2024-02-12 23:20:04 -0300

    feat: add PlaceItem component

---
+++ components/PlaceItem.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { View, Text, Pressable, StyleSheet } from 'react-native'
+
+function PlaceItem({place, onSelect}) {
+  return (
+    <Pressable onPress={onSelect}>
+    <Image source = {{uri: place.imageUrl}}/>
+    <View>
+        <Text>{place.title}</Text>
+        <Text>{place.address}</Text>
+
+    </View>
+
+    </Pressable>
+  )
+}
+
+export default PlaceItem
+
+
+const styles = StyleSheet.create({
+    
+})
