baf96eeadda54e5f6d718c41e5af7c17269c1ed6
Gabriel Bastos <gbastosrabelo@gmail.com>
2024-02-12 23:21:44 -0300

    feat: add AllPlace Screen

---
+++ screens/AllPlace.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View } from 'react-native'
+import PlacesList from '../components/PlacesList'
+
+function AllPlace() {
+  return (
+    <PlacesList/>
+  )
+}
+
+export default AllPlace
