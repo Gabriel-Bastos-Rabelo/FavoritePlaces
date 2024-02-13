04365b3ab0044412e2b10d1ca9281100bd1ecceb
Gabriel Bastos <gbastosrabelo@gmail.com>
2024-02-12 23:17:54 -0300

    feat: add stack navigation on App.js

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AllPlace from './screens/AllPlace';
+import AddPlace from './screens/AddPlace';
+import IconButton from './components/UI/IconButton';
+import { Colors } from './constants/Colors';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+    <StatusBar style='auto'/>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{
+        headerStyle: {backgroundColor: Colors.primary500},
+        headerTintColor: Colors.gray700,
+        contentStyle: {backgroundColor: Colors.gray700}
+      }}>
+        <Stack.Screen name = "AllPlaces" component={AllPlace} options={({navigation}) => 
+        ({
+          title: "Your favorite places",
+          headerRight: ({tintColor}) => <IconButton icon = "add" size = {24} color ={tintColor} onPress={() => navigation.navigate("AddPlace")}/>
+        })}/>
+        <Stack.Screen name = "AddPlace" component={AddPlace} options={{title: "Add a new place"}}/>
+      </Stack.Navigator>
+
+    </NavigationContainer>
+    </>
   );
 }
 
